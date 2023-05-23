f4b53ddd8d5d46023693a5fde116e15a89bb9b7e
lysgut <ltuninetticivarolo@escuelasprsproa.edu.ar>
2023-05-23 12:00:00 -0300

    |

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSW
 
 mongoose.connect(DB, {
 useNewUrlParser: true,
+
 })
 .then((con) => {
-console.log(con.connections);
-console.log("Connected to database");
+//console.log(con.connections);
+console.log("Conectado a la base de datos correctamente");
 });
 
 const port = 3000;
